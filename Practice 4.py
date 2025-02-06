47f54a3e6b6cfbaa62e19afd21c419fee5ebb73b
Amit kumar <118460550+amitvk92@users.noreply.github.com>
2025-02-06 18:24:26 +0530

    Add files via upload

---
+++ Practice 4.py
@@ -0,0 +1,24 @@
+g="my Name is Amit \/123456789"
+ 
+print(g)
+
+
+g = "My Name is Amit \n123456789"
+print(g)
+
+
+name =input("enter your name")
+print(f"good afternoon,{name}")
+
+name =input("enter your name")
+print({f}name)
+
+name = input("Enter your name")
+print("Good afternoon,{}".format(name))
+
+Letter = ''' Dear <|Name|>,
+            You are selected !
+            <|Date|> '''
+
+print(Letter.replace("<|Name|>")),
+("harry").replace("<|Date|","24 September 2050"))
