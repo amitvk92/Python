830cbc3710c06000945fb8aeb85cc464bff0727c
Amit kumar <118460550+amitvk92@users.noreply.github.com>
2025-02-11 18:46:09 +0530

    Add files via upload

---
+++ Practice_5.py
@@ -0,0 +1,19 @@
+letter = ''' Dear <|Name|>,
+            You are selected !
+            <|Date|> '''
+
+print(letter.replace("<|Name|>", "Amit Kumar").
+      replace("<|Date|>", "Feb 2025"))
+
+
+name ="Amit is ku  umar"
+postion = name.find("  "), name.replace("ku  umar, "kumar")
+corrected_ame = 
+
+print(,replace.("Kumar"))
+
+name = "harry is a good boy"
+print(name.find("i"))
+
+name= "harry is a  good boy  and bad  also"
+print(name.replace("  ","1"))
