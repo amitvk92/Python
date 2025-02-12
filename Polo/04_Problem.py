590d76632a7f9aa1bcf0105b2b1c7a5a8e44b73c
Amit kumar <118460550+amitvk92@users.noreply.github.com>
2025-02-12 20:47:03 +0530

    Add files via upload

---
+++ Polo/04_Problem.py
@@ -0,0 +1,18 @@
+a=34
+b=80
+print(a>b)
+
+a=5
+b=23
+#print(a>b)
+
+
+#a= int(input("enter number 1: "))
+#b= int(input("enter number 2: "))
+
+print("a is greater than b is", a>b)
+print("a is greater than b is", a<b)
+
+a=52
+b=62
+print()
